0b15afcbc016cef0f5f9cbdb5766792513f46216
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2016-01-26 00:56:56 +0100

    formating

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,10 +2,8 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     storyboard = UIStoryboard.storyboardWithName("Discourse", bundle: nil)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-
     @window.rootViewController = storyboard.instantiateInitialViewController
     @window.makeKeyAndVisible
-
     true
   end
 end
